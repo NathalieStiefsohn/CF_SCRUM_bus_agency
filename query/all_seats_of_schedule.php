223c213223cbd8f7525242e48633272ac9797683
Lio Kaufman <lio.kaufman@gmail.com>
2017-04-27 14:11:18 +0200

    query improvements

--- query/all_seats_of_schedule.php
+++ query/all_seats_of_schedule.php
@@ -1,9 +1,22 @@
 <?php
-$allSeatsOfSchedule = "SELECT sea.num AS number, discount_id, dis.rate AS discount, row, col, res.id IS NOT NULL AS booked FROM schedule AS sch
-INNER JOIN route AS rou ON sch.route_id = rou.id
-INNER JOIN bus AS bu ON rou.bus_id = bu.id
-INNER JOIN model AS mo ON bu.model_id = mo.id
-INNER JOIN seat AS sea ON mo.id = sea.model_id
-INNER JOIN discount as dis ON sea.discount_id = dis.id
-LEFT JOIN reservation AS res ON sea.id = res.seat_id
-WHERE sch.id = ?";
+$allSeatsOfSchedule = 
+"
+    SELECT 
+        sea.num AS number, 
+        discount_id, 
+        dis.rate AS discount, 
+        row, 
+        col, 
+        res.id IS NOT NULL AS booked 
+    FROM 
+        schedule AS sch
+        INNER JOIN route AS rou ON sch.route_id = rou.id
+        INNER JOIN bus AS bu ON rou.bus_id = bu.id
+        INNER JOIN model AS mo ON bu.model_id = mo.id
+        INNER JOIN seat AS sea ON mo.id = sea.model_id
+        INNER JOIN discount as dis ON sea.discount_id = dis.id
+        LEFT JOIN reservation AS res ON sea.id = res.seat_id
+    WHERE 
+        sch.id = ?
+";
+?>
