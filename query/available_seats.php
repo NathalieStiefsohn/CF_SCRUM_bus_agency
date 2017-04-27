f45b5cb7c81560d05989254899bfd830c4f6abc8
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-04-27 14:10:14 +0200

    add query to show the available seats for a schedule

---
+++ query/available_seats.php
@@ -0,0 +1,12 @@
+<?php
+
+$availableSeats = <<<SQL
+SELECT count(reservation.id) AS reservations FROM schedule 
+INNER JOIN route ON schedule.route_id = route.id
+INNER JOIN bus ON route.bus_id = bus.id
+INNER JOIN model ON bus.model_id = model.id
+INNER  JOIN seat ON model.id = seat.model_id
+INNER JOIN reservation ON seat.id = reservation.seat_id
+WHERE schedule.id = 1;
+
+SQL;
