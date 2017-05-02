bd5bff913dd7c7a747c2082fe75febe44bdb40cb
NathalieStiefsohn <nathalie.stiefsohn@hotmail.com>
2017-05-02 08:57:19 +0200

    finishes project except page 'offers' and 'risky rides'

--- users_of_ride.php
+++ users_of_ride.php
@@ -7,7 +7,7 @@ require_once('includes/start_session_admin.php');
 	$hidden="";
 	$count_all_users = "";
 	$count_users = "";
-	require'query/all_users_query.php';
+	
 
 	if ( isset($_GET['btn-search']) ){
 		$search = trim($_GET['search']);
@@ -21,7 +21,8 @@ require_once('includes/start_session_admin.php');
 ?>
 <?php
 	if(isset($_GET['schedule_id'])){
-		$schedule = $_GET['schedule_id'];
+		$schedule_id = $_GET['schedule_id'];
+		require'query/all_users_of_ride_query.php';
 		
 		
 	} else {
@@ -54,7 +55,8 @@ require_once('includes/head_tag.php');
 	    <div class="collapse navbar-collapse">
 	      <ul class="nav navbar-nav">
 	        <li><a href="home_admin.php">Risky Rides</a></li>
-	        <li class="active"><a href="all_rides.php">All Rides</a></li>
+	        <li class=""><a href="all_rides.php">All Rides</a></li>
+	        <li class="active"><a href="users_of_ride.php">Users of Ride</a></li>
 	        <li><a href="all_users.php">All Users</a></li>
 	        <li><a href="add_holidays.php">Manage Promo Days</a></li>
 	        <li><a href="add_schedules.php">Add Schedules</a></li>
@@ -102,9 +104,6 @@ require_once('includes/alert_box.php');
 
 			<div class="<?php echo $hidden; ?>">
 
-<?php
-require_once('includes/search_bar.php');
-		?>
 
 		
 		<div class="col-xs-12">
@@ -124,63 +123,7 @@ require_once('includes/search_bar.php');
 			    <tbody>
 			<?php 			
 				 // select all available users
-				 
-				if ( isset($_GET['btn-search']) ){
-					
-
-					if ($count_users == 1){
-						echo "<h4 class='text-center'>We found ".$count_users." result for '".$search."'.</h4> <hr>";
-					} else if ($count_users == 0) {
-					echo '<div class="alert alert-danger">
-							<h4 class="text-center">Unfortunately there are no results for "'.$search.'". <br></h4> 
-						</div><hr>';
-					} else {
-						echo "<h4 class='text-center'>We found ".$count_users." results for '".$search."'.</h4> <hr>";
-					}
-					$i = 0;
-			  		while($row_users = mysqli_fetch_array($res_users)){
-				  		$title = $row_users['title'];
-				  		$first_name = $row_users['first_name'];
-				  		$family_name = $row_users['last_name'];
-				  		$email = $row_users['email'];
-				  		$iban = $row_users['iban'];
-				  		$tel = $row_users['tel'];
-				  		$admin_id = $row_users['admin_id'];
-				  		$user_id = $row_users['user_id'];
-				  		
-				  	
-				  		echo 	'<tr> 
-									<td>'.$title.'</td>
-									<td>'.$first_name.'</td>
-									<td>'.$family_name.'</td>
-									<td>'.$email.'</td>
-									<td>'.$tel.'</td>
-									<td>'.$iban.'</td>
-									<td class="text-center">';
-
-						if (empty($admin_id)) {
-							echo '<form method="post" action="all_users.php?user_id='.$user_id.'">
-											<input type="submit" class="btn btn-success" value="Give" id="btn-give_admin_rights" name="btn-give_admin_rights">
-										</form>
-						  	';
-						} else {
-							echo '<form method="post" action="all_users.php?remove_rights_user_id='.$user_id.'">
-									<input type="submit" class="btn btn-primary background_bc1" value="Remove" id="btn-remove_admin_rights" name="btn-remove_admin_rights">
-								</form>
-						  	';
-						}
-						echo '		</td>
-									<td>
-										<form method="post" action="users_rides_admin.php?user_id_rides='.$user_id.'">
-											<input type="submit" class="btn btn-primary background_bc1" value="Rides" id="btn-users_rides" name="btn-users_rides">
-										</form>
-									</td>
-
-								</tr>';
-			  		}
-				} else {
-
-					
+				 		
 
 					
 			  		while($row_all_users = mysqli_fetch_array($res_all_users)){
@@ -222,7 +165,7 @@ require_once('includes/search_bar.php');
 
 								</tr>';
 			  		}
-  				}
+  				
 	  				
 			?>
 				</tbody>
